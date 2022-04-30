c7e99c6a52f3e4ddcd42ea64e150fa2395f9637f
Jo Flow <JoFlowCode@gmail.com>
2022-04-29 17:56:25 -0700

    attached .js file

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+let username = prompt('How lovely of you to stop by. What shall we call you?');
+console.log(userName);
+
+if (username === "Diamond Gold Emerald Platinum Member") {
+    console.log("Welcome back esteemed guest."); 
+}
+
+} else {
+    console.log("That username isn't recognized but we welcome you.");
+
+let animal = prompt ('What is your favorite animal?');
+let message; 
+
+if(animal === "cat") {
+    console.log("You have excellent taste. Rememebr the code 1234 for acces to our super secret vault collection.");
+
+} else {
+    console.log("You have good taste.");
+}
