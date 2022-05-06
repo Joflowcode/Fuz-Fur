5496e1c66aab0e45d03f01631705f87c27fe903c
Jo Flow <JoFlowCode@gmail.com>
2022-05-05 17:08:31 -0700

    fixed JS prompt for welcome

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-'use strict'
+'use strict'; 
+
+let username = prompt ("What is your name?");
+console.log(username);
 
 // declare a variable, assign it the value of the user's input
-var MembershipStatus = prompt("Hello, Welcome to Fuz & Fur Fine Art & Masterpieces. Are you a sponsor?");
- 
-// print the user's input to the console
-console.log(MembershipStatus);
+let MembershipStatus = prompt("Hello, " + username + ". Are you a sponsor of Fuz & Fur Masterpiece Collection?");
+
 
 // checking is user is a member or new site visitor
-if (MembershipStatus === "yes") {
+if (MembershipStatus === "yes" || "yeah" || "ye" || "yas"){
     console.log("Welcome back to Fuz & Fur. You are going to love our new collection");
+   // what to do if they are not a sponsor member
+} else if (MembershipStatus === "no" || "nah" || "n") {
+    console.log("Welcome to Fuz & Fur Fine Art & Masterpiece Collection. Feel contact us at our email if need curator or art agent assistance. We are glad to have you.");
+
+} else {
+    console.log("Welcome to Fuz & Fur Fine Art & Masterpiece Collection. Feel contact us at our email if need curator or art agent assistance. We are glad to have you.");
 }
 
-// what to do if they are not a sponsor member
-else {
-    console.log("Welcome to Fuz & Fur Fine Art & Masterpiece Collection. Feel contact us at our email if need curator or art agent assistance. We're glad to welcome you.")
-}
+
+
+document.write('Hello ' + username)
 
 // selects the HTML button element from our document
-var button =
-document.querySelector( 'button')
+
 
 
 
