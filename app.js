5d2026d157dd73c9c66f53cfc5f970e2eca763c7
Jo Flow <JoFlowCode@gmail.com>
2022-05-05 17:29:35 -0700

    fixed case sensitivity for username vs userName error

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 'use strict'; 
 
-let username = prompt ("What is your name?");
-console.log(username);
+let userName = prompt ("What is your name?");
+console.log(userName);
+
+if (userName === "Jo Flow") {
+    console.log("Hello Admin");
+
+} else {
+    console.log("Hello user");
+}
 
 // declare a variable, assign it the value of the user's input
-let MembershipStatus = prompt("Hello, " + username + ". Are you a sponsor of Fuz & Fur Masterpiece Collection?");
-
+let MembershipStatus = prompt("Hello, " + userName + ". Are you a sponsor of Fuz & Fur Masterpiece Collection?");
+console.log(MembershipStatus)
 
 // checking is user is a member or new site visitor
 if (MembershipStatus === "yes" || "yeah" || "ye" || "yas"){
     console.log("Welcome back to Fuz & Fur. You are going to love our new collection");
+    
    // what to do if they are not a sponsor member
 } else if (MembershipStatus === "no" || "nah" || "n") {
     console.log("Welcome to Fuz & Fur Fine Art & Masterpiece Collection. Feel contact us at our email if need curator or art agent assistance. We are glad to have you.");
@@ -20,7 +28,7 @@ if (MembershipStatus === "yes" || "yeah" || "ye" || "yas"){
 
 
 
-document.write('Hello ' + username)
+document.write('Hello ' + userName)
 
 // selects the HTML button element from our document
 
